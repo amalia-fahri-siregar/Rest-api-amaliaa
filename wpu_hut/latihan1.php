fb16157e465606f21823044db29f53ddb60f1fa2
Amalia Fahri Siregar <amaliafahrisrg@gmail.com>
2025-05-29 14:56:38 +0700

    light

--- wpu_hut/latihan1.php
+++ wpu_hut/latihan1.php
@@ -1,14 +1,9 @@
 <?php
-$dtaa = file_get_contents('data/pizza.json');
+$data = file_get_contents('data/pizza.json');
 $menu = json_decode($data, true);
 
-
 $menu = $menu["menu"];
-
 ?>
-
-
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -19,67 +14,73 @@ $menu = $menu["menu"];
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <title>WPU Hut</title>
+    <title>WPU Hut!</title>
   </head>
   <body>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
       <a class="navbar-brand" href="#">
-       <img src="img/logo.png" alt="">
-    </a>
+          <img src="img/logo.png" width="120">
+      </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <a class="nav-link active" href="#">Home</a>
-        </div>
       </div>
     </div>
-  </nav>
+  </div>
+</nav>
 
-    <div class="container">
-        
+  <div class="container">
+
       <div class="row mt-3">
-        <div class="class">
+        <div class="col">
           <h1>All Menu</h1>
-      </div>
-    </div>
-
-<div class="row">
-            <?php foreach ($menu as $row) : ?>
-            <div class="col-md-4">
-                <div class="card mb-3">
-                    <img src="img/menu/<?= $row["gambar"];?>" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $row["nama"];?></h5>
-                        <p class="card-text"><?= $row["deskripsi"];?></p>
-                        <h5 class="card-title">Rp. <?= $row["harga"];?></h5>
-                        <a href="#" class="btn btn-primary">Pesan Sekarang</a>
-                    </div>
-                </div>
-            </div>
-            <?php endforeach; ?>
         </div>
+      </div>
 
-    </div>
+      <div class="row">
+        <?php foreach ($menu as $row) : ?>
+          <div class="col-md-4">
+              <div class="card">
+                  <img src="img/menu/american-favourite.jpg" class="card-img-top">
+                  <div class="card-body">
+                      <h5 class="card-title">American Favourite</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <h5 class="card-title">Rp. 60.000,-</h5>
+                      <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+                  </div>
+              </div>
+          </div>
+          <?php endforeach; ?>
+      </div>
+
+
+  </div>
+
+
+
+
+
+
+
+
 
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-    
+    <script
+    src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+    crossorigin="anonymous"></script>
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
-    <script 
-        src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" 
-        integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" 
-        crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
     -->
   </body>
