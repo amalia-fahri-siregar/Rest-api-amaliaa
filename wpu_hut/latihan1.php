9acae66f251ba4ab40d5ee61867ac5fd0a077fd9
Amalia Fahri Siregar <amaliafahrisrg@gmail.com>
2025-05-29 15:17:37 +0700

    Home

--- wpu_hut/latihan1.php
+++ wpu_hut/latihan1.php
@@ -28,7 +28,7 @@ $menu = $menu["menu"];
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-link active" href="#">Home</a>
+          <a class="nav-link active" href="#">All Menu</a>
       </div>
     </div>
   </div>
@@ -45,13 +45,14 @@ $menu = $menu["menu"];
       <div class="row">
         <?php foreach ($menu as $row) : ?>
           <div class="col-md-4">
-              <div class="card">
-                  <img src="img/menu/american-favourite.jpg" class="card-img-top">
+              <div class="card mb-3">
+                  <img src="img/menu/<?=$row["gambar"]; ?>" 
+                  class="card-img-top">
                   <div class="card-body">
-                      <h5 class="card-title">American Favourite</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <h5 class="card-title">Rp. 60.000,-</h5>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                      <h5 class="card-title"><?=$row["nama"]; ?></h5>
+                      <p class="card-text"><?=$row["deskripsi"]; ?></p>
+                      <h5 class="card-title">Rp. <?=$row["harga"]; ?></h5>
+                      <a href="#" class="btn btn-primary">Pesan Sekarang</a>
                   </div>
               </div>
           </div>
